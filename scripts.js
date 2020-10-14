2840efad92f27490a5ec0fa5d03c828ac0cf2680
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-10-14 10:06:18 -0500

    changes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,22 @@
 // Write your JavaScript code here.
+window.addEventListener("load",function(){
+let button_take=getElementById("takeoff");
+
+function confirm() {
+ window.confirm("Confirm that the shuttle is ready for takeoff.");
+}
+
+
+button_take.addEventListener("click",confirm{
+ let flightStatus=getElementById("flightStatus");
+ flightStatus.innerHTML="Shuttle in flight.";
+ let shuttle_Background=getElementById("shuttleBackground");
+ shuttle_Background.style.background-color=blue;
+
+
+
+});
+
+
+});
 // Remember to pay attention to page loading!
