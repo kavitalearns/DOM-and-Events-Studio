ef08da2858e4278cfd9336eb424726f807a29568
kavitalearns <kavitabansal_01@yahoo.co.in>
2020-10-14 13:18:35 -0500

    commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 // Write your JavaScript code here.
+let response=null;
+let flight_Status=null;
+window.addEventListener("load",function(){
+response=document.getElementById("takeoff").addEventListener('click',function() { confirm("Confirm that the shuttle is ready for takeoff.");});
 
-document.getElementById("takeoff").addEventListener('click',function() { confirm("hi");});
+ if (response){
+ document.getElementById("flightStatus").innerHTML="Shuttle in flight.";
+ }
 
-
+});
 // Remember to pay attention to page loading!
